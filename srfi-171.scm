dd2edb97eed3aab1f4ea6429d4b9965001550669
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-07 07:40:39 -0800

    Checked in srfi-171.scm

---
+++ srfi-171.scm
@@ -0,0 +1,42 @@
+(module srfi-171 ()
+  (import scheme)
+  (import (chicken base))
+  (import (only vector-lib vector->list))
+  (import srfi-1)
+  (import srfi-69)
+  (import (r6rs bytevectors))
+  (import (chicken module))
+  (export rcons
+          reverse-rcons
+          rcount
+          rany
+          revery
+          list-transduce
+          vector-transduce
+          string-transduce
+          bytevector-u8-transduce
+          port-transduce
+          generator-transduce
+
+          tmap
+          tfilter
+          tremove
+          treplace
+          tfilter-map
+          tdrop
+          tdrop-while
+          ttake
+          ttake-while
+          tconcatenate
+          tappend-map
+          tdelete-neighbor-duplicates
+          tdelete-duplicates
+          tflatten
+          tsegment
+          tpartition
+          tadd-between
+          tenumerate
+          tlog)
+
+(include "srfi/srfi-171-meta.scm")
+(include "srfi/171-impl.scm"))
